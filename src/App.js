034ab91cdeb09de04767758a606954b23096fbd4
arifemrecelik <ce.arifemre@gmail.com>
2018-12-20 16:49:53 +0300

    Refactor

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import { HotTable } from "@handsontable/react";
+import "handsontable/dist/handsontable.full.css";
+import { Modal } from "antd";
+import { Button } from "antd";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.myTable = React.createRef();
+    this.state = {
+      modalVisible: false
+    };
+  }
   render() {
+    const myData = [[1, 1, 1, 1, 1]];
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div style={{ padding: "30px" }}>
+        <Button
+          onClick={() => {
+            this.setState(prevState => ({
+              modalVisible: !prevState.modalVisible
+            }));
+          }}
+        >
+          TEST
+        </Button>
+        <Modal visible={this.state.modalVisible}>
+          <HotTable
+            ref={this.myTable}
+            data={myData}
+            width='500'
+            height='500'
+            colHeaders
+            rowHeaders
+          />
+        </Modal>
       </div>
     );
   }
